6e3bd435f2d8b8a44dbc74465167e422d2da59e7
nocope <nocodeprogramming@gmail.com>
2021-10-18 22:05:12 +0200

    jthread explanations

--- thread/jthread.cpp
+++ thread/jthread.cpp
@@ -26,5 +26,9 @@ int main()
 //jthread 
 //automatically, joins on its destructor
 
+//more preciesly, jthread work with stop_token
+//the function to be called must have std::stop_token as an argument
+//it must use for its process logic to stop immediately when the signal is on.
+
 //compiler may need to be updated
 //check with C++20 
