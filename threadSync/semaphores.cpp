169c1dde83420ab6466f71898e9a70c07d464e60
nocope <nocodeprogramming@gmail.com>
2021-10-21 00:05:03 +0200

    semaphore

--- threadSync/semaphores.cpp
+++ threadSync/semaphores.cpp
@@ -1,49 +1,65 @@
+// to explain C++ semaphores, we need to understand semarphoer concept itself
+
+//binary semaphore? counting semaphore
+
+// not bound to thread? how?
+//is there a thread pool in it?
+//memory managments?
+
+//and compare with mutex
+
 #include <iostream>
-#include <mutex>
-#include <condition_variable>
 #include <thread>
-
-std::mutex mtx;
-std::condition_variable cv;
-
-bool ready = false;
-
-void waitandDo()
-{
-	//here unique_lock is better? why?std::lock_guard<std::mutex> lck(mtx);
-	//if we frequently calls, it unique_lock is better?
-	//why lock guard is not working??
-	std::unique_lock<std::mutex> lck(mtx);
-	//very imporant to check ready (lostwakdup/sprious wakeup)
-	//it checks ready and lock later. (to prevent lost wake up)
-	//lost wake up : when signal sent before condition lock
-	//sprious wake up?
-	cv.wait(lck, []{ return ready; });
-	std::cout << "work" << std::endl;
+#include <chrono>
+#include <semaphore>
+ 
+ 
+// global binary semaphore instances
+// object counts are set to zero
+// objects are in non-signaled state
+std::binary_semaphore
+	smphSignalMainToThread(0),
+	smphSignalThreadToMain(0);
+ 
+void ThreadProc()
+{	
+	// wait for a signal from the main proc
+	// by attempting to decrement the semaphore
+	smphSignalMainToThread.acquire();
+ 
+	// this call blocks until the semaphore's count
+	// is increased from the main proc
+ 
+	std::cout << "[thread] Got the signal\n"; // response message
+ 
+	// wait for 3 seconds to imitate some work
+	// being done by the thread
+	using namespace std::literals;
+	std::this_thread::sleep_for(3s);
+ 
+	std::cout << "[thread] Send the signal\n"; // message
+ 
+	// signal the main proc back
+	smphSignalThreadToMain.release();
 }
-
-
-void sender()
-{
-	{ //very important to lock 
-		std::lock_guard<std::mutex> lck(mtx);
-		ready = true;
-	}
-	cv.notify_one();
-}
-
+ 
 int main()
 {
-
-    std::thread t1(waitandDo);
-	std::thread t2(sender);
-	
-	//join is must
-	t1.join();
-	t2.join();
-
-	std::cout << "main thread finish" << std::endl;
-
+	// create some worker thread
+	std::thread thrWorker(ThreadProc);
+ 
+	std::cout << "[main] Send the signal\n"; // message
+ 
+	// signal the worker thread to start working
+	// by increasing the semaphore's count
+	smphSignalMainToThread.release();
+ 
+	// wait until the worker thread is done doing the work
+	// by attempting to decrement the semaphore's count
+	smphSignalThreadToMain.acquire();
+ 
+	std::cout << "[main] Got the signal\n"; // response message
+	thrWorker.join();
 }
 
 //producer/consumer, 
